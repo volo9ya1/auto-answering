3d73f90d4aca33034652b24169752518ae033fd7
Kirel <kirillgulyamov2009@gmail.com>
2026-08-20 14:04:06 +0500

    Add HTTP server and update bot message

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Bot } = require("grammy");
+const http = require("http");
 
-// Получаем токен из переменных окружения
+// Век-заглушка для Render
+http.createServer((req, res) => res.end("Bot is running!")).listen(process.env.PORT || 3000);
+
+// Ваш бот
 const bot = new Bot(process.env.BOT_TOKEN);
 
-// Обработка бизнес-сообщений из профиля
 bot.on("business_message", async (ctx) => {
-  await ctx.reply("Приветствую! Сейчас я не в сети,когда зайду в сеть, я обязательно прочту.Если хотите передать что-то срочное то лучше позвоните мне. ");
+  await ctx.reply("Приветствую! Сейчас я не в сети,когда зайду в сеть, я обязательно прочту.Если хотите передать что-то срочное то лучше позвоните мне.");
 });
 
 bot.start();
